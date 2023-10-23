1e2ffc740f1932509b78dbad7d454b4d10f44d47
Mizosu97 <earthpig97@gmail.com>
2023-10-23 14:40:43 -0400

    cat

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,9 +6,16 @@ export default defineConfig({
 	site: 'https://the-duat.github.io',
 	integrations: [
 		starlight({
-			title: 'My Docs',
+			title: 'The Duat',
+			customCss: [
+				// Relative path to your custom CSS file
+				'./src/styles/cat.css',
+			],
+			logo: {
+				src: './src/assets/eye.png',
+			},
 			social: {
-				github: 'https://github.com/withastro/starlight',
+				github: 'https://github.com/the-duat/mizos',
 			},
 			sidebar: [
 				{
