0130e45330caa33c538e5a4e1a72fd84ff44692a
Mizosu <97634559+Mizosu97@users.noreply.github.com>
2024-04-05 17:04:16 -0400

    uwu

--- public/repo.lua
+++ public/repo.lua
@@ -1,12 +1,9 @@
--- mizOS package repository
+-- Package repository for the Osiris Package Management System, package manager used for NILE add-ons, themes, and plugins.
 -- If you see a package in here that isn't listed on the website, it is most likely either broken or still in development. Don't try to install them if you don't know what you're doing.
 
 packages = {
 	["official"] = {
-		["the-duat/mizos-asusctl"] = {"the-duat/mizos-asusctl", true},
-		["the-duat/mizos-supergfxctl"] = {"the-duat/mizos-supergfxctl", true},
-		["mizosu97/grapejuice"] = {"mizosu97/grapejuice", true},
-		["mizosu97/bastet"] = {"mizosu97/bastet", true}
+		["the-duat/nile-dark-theme"] = {"the-duat/nile-dark-theme", true}
 	},
 
 	["community"] = {
