6865f1645a97e170c6e6260f104a4d3666e135b6
Mizosu97 <earthpig97@gmail.com>
2024-07-28 00:19:31 -0400

    uwu

--- astro.config.mjs
+++ astro.config.mjs
@@ -22,18 +22,42 @@ export default defineConfig({
 				{
 					label: 'Lua Software',
 					items: [
-						{ label: 'Installation Script', link: '/install/install-script/' },
+						{ label: 'About', link: '/luasoftware/about/' },
+						{ label: 'N.I.L.E.', link: '/luasoftware/nile/' },
+						{ label: 'Wedjat', link: '/luasoftware/wedjat/' },
+						{ label: 'ThirdEyeSearch', link: '/luasoftware/thirdeyesearch/' },
+						{ label: 'Set', link: '/luasoftware/set/' },
 					],
 				},
 				{
-					label: 'River Frontend',
+					label: 'Other Software',
 					items: [
-						{ label: 'The Basics', link: '/river/basics/' },
-						{ label: 'System Info', link: '/river/displayinfo/' },
-						{ label: 'System Configuration', link: '/river/configuration/' },
-						{ label: 'Init Systems', link: '/river/initsystems/' },
-						{ label: 'Software Management', link: '/river/software/' },
-						{ label: 'System Updates', link: '/river/updates/' },
+						{ label: 'About', link: '/othersoftware/about/' },
+						{ label: 'LimitedsFiend', link: '/othersoftware/limitedsfiend/' },
+					],
+				},
+				{
+					label: 'Roblox Commissions',
+					items: [
+						{ label: 'About', link: '/robloxcommissions/about/' },
+						{ label: 'Donation Depot', link: '/robloxcommissions/donationdepot/' },
+					],
+				},
+				{
+					label: 'Roblox Games',
+					items: [
+						{ label: 'About', link: '/robloxgames/about/' },
+						{ label: 'Pharaohs Landing', link: '/robloxgames/pharaohslanding/' },
+						{ label: 'Prison', link: '/robloxgames/prison/' },
+					],
+				},
+				{
+					label: 'Roblox Prototypes',
+					items: [
+						{ label: 'About', link: '/robloxprototypes/about/' },
+						{ label: 'RayBlox', link: '/robloxprototypes/rayblox/' },
+						{ label: 'Spelunky Generation', link: '/robloxprototypes/spelunkygen/' },
+						{ label: 'Tank Game', link: '/robloxprototypes/tankgame/' },
 					],
 				},
 			],
